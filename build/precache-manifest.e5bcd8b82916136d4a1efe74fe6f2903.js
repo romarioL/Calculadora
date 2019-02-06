fe292bc8c983cb8f557747be847101d13b3b4471
romarioL <45340806+romarioL@users.noreply.github.com>
2019-02-06 04:51:20 -0300

    Add files via upload

---
+++ build/precache-manifest.e5bcd8b82916136d4a1efe74fe6f2903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b93c2ad436ccb747ae415c1e865819a4",
+    "url": "/static/media/RobotoMono-Thin.b93c2ad4.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b4ed4d1ed870644df65",
+    "url": "/static/js/main.0b4ed4d1.chunk.js"
+  },
+  {
+    "revision": "956d88ae2b3c74b5d6bc",
+    "url": "/static/js/1.956d88ae.chunk.js"
+  },
+  {
+    "revision": "0b4ed4d1ed870644df65",
+    "url": "/static/css/main.48190080.chunk.css"
+  },
+  {
+    "revision": "96d583094319d089528d78e6be6bc348",
+    "url": "/index.html"
+  }
+];
